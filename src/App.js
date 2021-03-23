35728e25d8b52c93e5c202f06e11a5d1a7caf874
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-03-22 23:08:07 -0400

    ADD: CSS applied

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <Product title="Amazon echo" price={19.99} />
+      <Product title="Samsung TV" price={299.99} />
+
+      <Product title="Iphone Apple 12" price={1200} />
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.css";
 
 function Product({ title, price }) {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="product">
       <h2>{title}</h2>
       <h3>Price: ${price}</h3>
+      <button onClick={(e) => setCount(count + 1)}>+</button>
+      <h4>{count}</h4>
+      <button onClick={(e) => setCount(count - 1)}>-</button>
     </div>
   );
 }
